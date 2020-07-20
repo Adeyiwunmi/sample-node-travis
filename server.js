6cc27c6867ac25b51dcf3a74c20be0a1e4a78adc
Adeyiwunmi Adebolu <>
2020-07-20 17:21:05 +0100

    added new env variable to test

--- server.js
+++ server.js
@@ -6,6 +6,8 @@ function sleep(ms) {
 async function main() {
   while(true) {
     console.log("Microservices rock!!");
+    const favoriteFood = process.env.FAV_FOOD;
+    console.log(`My favorite food is ${favoriteFood}`);
     await sleep(5000);
   }
 }
